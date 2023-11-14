f0579db33336da3697f4d2fb41c0fb85a4deef4d
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-11-14 20:11:31 +0100

    commit

---
+++ src/ait/employee/dao/CompanyListImpl.java
@@ -0,0 +1,121 @@
+package ait.employee.dao;
+
+import ait.employee.model.Employee;
+import ait.employee.model.SalesManager;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class CompanyListImpl implements Company {
+    private List<Employee> employees;
+    private int capacity;
+
+    public CompanyListImpl(int capacity) {
+        this.capacity = capacity;
+        employees = new ArrayList<>();
+    }
+
+    // O(n)
+    @Override
+    public boolean addEmployee(Employee employee) {
+        if (employee == null) {
+            throw new RuntimeException("null");
+        }
+        if (capacity == employees.size() || employees.contains(employee)) {
+            return false;
+        }
+        return employees.add(employee);
+    }
+
+    // O(n)
+    @Override
+    public Employee removeEmployee(int id) {
+        Employee employee = findEmployee(id);
+        employees.remove(employee);
+        return employee;
+    }
+
+    // O(n)
+    @Override
+    public Employee findEmployee(int id) {
+        for (Employee employee : employees) {
+            if (employee.getId() == id) {
+                return employee;
+            }
+        }
+        return null;
+    }
+
+    // O(n)
+    @Override
+    public double totalSalary() {
+        double res = 0;
+        for (Employee employee : employees) {
+            res += employee.calcSalary();
+        }
+        return res;
+    }
+
+    // O(1)
+    @Override
+    public int quantity() {
+        return employees.size();
+    }
+
+    @Override
+    public double avgSalary() {
+        return 0;
+    }
+
+    // O(n)
+    @Override
+    public double totalSales() {
+        double res = 0;
+        for (Employee employee : employees) {
+            if (employee instanceof SalesManager) {
+                SalesManager salesManager = (SalesManager) employee;
+                res += salesManager.getSalesValue();
+            }
+        }
+        return res;
+    }
+
+    // O(n)
+    @Override
+    public void printEmployees() {
+        employees.forEach(e -> System.out.println(e));
+    }
+
+    @Override
+    public Employee[] findEmployeeHoursGreaterThan(int hours) {
+        return new Employee[0];
+    }
+
+    @Override
+    public Employee[] findEmployeeSalaryRange(int minSalary, int maxSalary) {
+        return new Employee[0];
+    }
+
+    // O(n)
+    @Override
+    public Employee[] findEmployeesHoursGreaterThan(int hours) {
+        return findEmployeesByPredicate(e -> e.getHours() >= hours);
+    }
+
+    // O(n)
+    @Override
+    public Employee[] findEmployeesSalaryRange(int minSalary, int maxSalary) {
+        return findEmployeesByPredicate(e -> e.calcSalary() >= minSalary && e.calcSalary() < maxSalary);
+    }
+
+    private Employee[] findEmployeesByPredicate(Predicate<Employee> predicate) {
+        List<Employee> res = new ArrayList<>();
+        for (Employee employee : employees) {
+            if (predicate.test(employee)) {
+                res.add(employee);
+            }
+        }
+        return res.toArray(new Employee[0]);
+    }
+}
